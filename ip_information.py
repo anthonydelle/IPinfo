2945b63a66a0acb75a99dce924fbd7ef0258d725
BuildTools <anthony.dellechiaie@gmail.com>
2018-04-18 22:44:42 -0400

    added userinput

--- ip_information.py
+++ ip_information.py
@@ -1,6 +1,16 @@
 from modules.IPinfo import IPinfo
+import socket
 
-a = IPinfo()
-print(a.ip())
+while True:
+    userinput = input("Please supply an IP address or press Enter to use your current IP")
 
+    if userinput:
+        try:
+            socket.inet_aton(userinput)
+            break
+        except socket.error:
+            print("Invalid IP")
 
+a = IPinfo(userinput)
+
+print(a.allinfo())
