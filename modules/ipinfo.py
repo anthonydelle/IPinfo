4e0e4b4fe6c011f75490dca46ee69f9c6c096b0e
BuildTools <anthony.dellechiaie@gmail.com>
2018-04-22 14:23:05 -0400

    converted methods in IPinfo to case attributes

--- modules/ipinfo.py
+++ modules/ipinfo.py
@@ -7,30 +7,10 @@ class IPinfo(object):
         self.ipwebsite = "https://ipinfo.io/"
         self.r = requests.get(self.ipwebsite + self.givenip)
         self.information = self.r.json()
-
-    def ip(self):
-        return self.information['ip']
-
-    def hostname(self):
-        return self.information['hostname']
-
-    def loc(self):
-        return self.information['loc']
-
-    def org(self):
-        return self.information['org']
-
-    def city(self):
-        return self.information['city']
-
-    def region(self):
-        return self.information['region']
-
-    def country(self):
-        return self.information['country']
-
-    def phone(self):
-        return self.information['phone']
-
-    def allinfo(self):
-        return self.information
+        self.ip = self.information["ip"]
+        self.hostname = self.information["hostname"]
+        self.city = self.information["city"]
+        self.region = self.information["region"]
+        self.county = self.information["county"]
+        self.loc = self.information["loc"]
+        self.org = self.information["org"]
